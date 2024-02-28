dd3ffb8a53e22f747234fb5fa4ad5fcd96b73d32
ntDat <cudeanhno@gmail.com>
2024-02-28 23:54:20 +0700

    add xml

--- app/src/main/java/com/example/superkeyboardkotlin/fragment/ShopFragment.kt
+++ app/src/main/java/com/example/superkeyboardkotlin/fragment/ShopFragment.kt
@@ -6,24 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.superkeyboardkotlin.R
+import com.example.superkeyboardkotlin.databinding.FragmentShopBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ShopFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ShopFragment : Fragment() {
-
+    private lateinit var binding :FragmentShopBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shop, container, false)
+    ): View {
+        binding = FragmentShopBinding.inflate(LayoutInflater.from(requireContext()))
+        initData()
+        return binding.root
+    }
+
+    private fun initData() {
+
     }
 }
